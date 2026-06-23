7bdd6a859abd4baf13ab5ea678d29e0448ff5e50
Asgar Ali <112402800+Asgarali1@users.noreply.github.com>
2026-06-23 10:45:51 +0000

    type convrsion effect

---
+++ basics/typeConversion.js
@@ -0,0 +1,17 @@
+let score = "33abc"
+
+let notAn = NaN
+console.log(typeof score);
+
+let newScore = Number(score); //convert string to number data type
+console.log(typeof newScore);
+
+console.log(newScore); //value is NaN
+
+console.log(typeof notAn);  //Type is Number
+
+
+
+
+
+
