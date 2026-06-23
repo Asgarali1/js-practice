10db7eb74faf0fd86883295073b87896eaff1696
Asgar Ali <112402800+Asgarali1@users.noreply.github.com>
2026-06-23 07:24:09 +0000

    add datatypes file

--- basics/01.js
+++ basics/01.js
@@ -1,1 +1,6 @@
-console.log("Asgar");
+const id =2923823
+let name = 'Asgar' 
+var age = 26
+city = 'vns'
+
+console.table([id, name, age, city])
---
+++ basics/02_dataTypes.js
@@ -0,0 +1,18 @@
+"use strict"; //This is use for everything use newer version of js
+
+/* ---Primitive-Data-Types-----
+number
+bigInt
+string
+boolean
+null -> standalone value
+undefined
+symbol -> unique
+
+----non-primitive-data-types-----
+Object
+console.log(TypeOf key or value) for finding the type of datatype
+
+and null datatype is object 
+and undefined is undefined
+*/
